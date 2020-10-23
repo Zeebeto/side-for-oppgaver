c37844ee1d038eff4a6297ef553b1f95cd7f23be
Zeebeto <72790252+Zeebeto@users.noreply.github.com>
2020-10-23 13:18:19 +0200

    øiljaSHEF

--- script.js
+++ script.js
@@ -51,3 +51,61 @@ function mouseOver(x) {
 function mouseOut(z) {
     z.innerHTML = `o`
 }
+
+//funksjoner for kodelås
+var kode = '';
+function rightcode() {
+    kode = document.getElementById('kodelåsdisplay').innerHTML = "";
+}
+function btnpnd() {
+    if (kode == '0308') {
+        document.getElementById("lockstatus").innerHTML = "open";
+    } else {
+        document.getElementById('lockstatus').innerHTML = "access denied";
+    }
+}
+function btn1() {
+    document.getElementById('kodelåsdisplay').innerHTML += '1';
+}
+
+function btn2() {
+    document.getElementById('kodelåsdisplay').innerHTML += '2';
+}
+
+function btn3() {
+    document.getElementById('kodelåsdisplay').innerHTML += '3';
+}
+
+function btn4() {
+    document.getElementById('kodelåsdisplay').innerHTML += '4';
+}
+
+function btn5() {
+    document.getElementById('kodelåsdisplay').innerHTML += '5';
+}
+
+function btn6() {
+    document.getElementById('kodelåsdisplay').innerHTML += '6';
+}
+
+function btn7() {
+    document.getElementById('kodelåsdisplay').innerHTML += '7';
+}
+
+function btn8() {
+    document.getElementById('kodelåsdisplay').innerHTML += '8';
+}
+
+function btn9() {
+    document.getElementById('kodelåsdisplay').innerHTML += '9';
+}
+
+function btn0() {
+    document.getElementById('kodelåsdisplay').innerHTML += '0';
+}
+
+function btnclear() {
+    document.getElementById('kodelåsdisplay').innerHTML = '';
+    document.getElementById('lockstatus').innerHTML = '';
+}
+
